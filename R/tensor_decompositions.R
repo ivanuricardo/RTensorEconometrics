e6bd3fbc1337c264727425eeaa699e6e90c6962c
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-05-02 17:27:44 +0200

    add cp rank selection

--- R/tensor_decompositions.R
+++ R/tensor_decompositions.R
@@ -23,3 +23,34 @@ reconstruct_cp <- function(A, B, C, r, lambda = rep(1, r)) {
   }
   return(tens)
 }
+
+#' Select the rank of a tensor using CP decomposition
+#'
+#' This function selects the rank of a tensor based on the ratio of the
+#' approximation error between different rank values obtained by CP decomposition.
+#' 
+#' @param tnsr The input tensor.
+#' @param max_rank The maximum rank to consider.
+#' @return The selected rank.
+#' @export
+cp_rank_selection <- function(tnsr, max_rank) {
+  # Check input types
+  if (!inherits(tnsr, "Tensor")) {
+    stop("tnsr must be a Tensor object.")
+  }
+  
+  fnorm_fit <- NULL
+  
+  for (i in 1:max_rank) {
+    # Compute CP decomposition with i components
+    sim_cp <- cp(tnsr, num_components = i)
+    fnorm_fit[i] <- sim_cp$fnorm_resid
+  }
+  
+  # Compute the ratio of approximation errors
+  vec_rank_ratio <- fnorm_fit[-max_rank] / fnorm_fit[-1]
+  
+  # Return the index of the minimum ratio
+  return(list(selected_rank = which.min(vec_rank_ratio), rank_vec = vec_rank_ratio))
+}
+
