395b9827b4f788be8b319b3a02a7b7b0431f0d2a
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-06-03 16:06:49 +0200

    remove data test

---
+++ R/tucker_tmp.R
@@ -0,0 +1,17 @@
+# Tucker Regression
+
+xt_regression <- function () {
+  
+}
+
+yt_regression <- function () {
+  
+}
+
+tuck_conv <- function() {
+  
+}
+
+tucker_regression <- function(Y, X, R, obs_dim_X, obs_dim_Y) {
+  
+}
