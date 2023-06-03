c9f5b8c61da89cd37411e71fe8f40f1736d136fa
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-06-03 16:47:26 +0200

    tucker skeleton

--- R/tucker_tmp.R
+++ R/tucker_tmp.R
@@ -30,10 +30,39 @@ tucker_regression <- function(Y, X, R, obs_dim_X, obs_dim_Y, convThresh = 1e-05,
   # Generate initial random tensor and random CP decomposition
   init_B <- rand_tensor(c(X@modes[-obs_dim_X], Y@modes[-obs_dim_Y]))
   init_list <- list(
-    matrix(rnorm(X@modes[2] * R), nrow = X@modes[2]),
-    matrix(rnorm(X@modes[3] * R), nrow = X@modes[3]),
-    matrix(rnorm(Y@modes[2] * R), nrow = Y@modes[2]),
-    matrix(rnorm(Y@modes[3] * R), nrow = Y@modes[3])
+      matrix(rnorm(X@modes[2] * R[1]), nrow = X@modes[2]),
+      matrix(rnorm(X@modes[3] * R[2]), nrow = X@modes[3]),
+      matrix(rnorm(Y@modes[2] * R[3]), nrow = Y@modes[2]),
+      matrix(rnorm(Y@modes[3] * R[4]), nrow = Y@modes[3])
   )
   
+  converged <- FALSE
+  num_iter <- 0
+  while (num_iter < max_iter) {
+    num_iter <- num_iter + 1
+    
+    for (dim in 1:init_B@num_modes) {
+      if (dim < (init_B@num_modes/2 + 1)) {
+        x_reg_list <- xt_regression(init_list = init_list, Y = Y, X = X,
+                                   R = R, idx = dim)
+        pre_init_list <- init_list
+        init_list[[dim]] <- x_reg_list$B1
+        lambdas <- x_reg_list$x_lambdas
+        if (convergence_func(pre_init_list, init_list, dim, convThresh)) {
+          converged <- TRUE
+          break
+        }
+      } else {
+        y_reg_list <- yt_regression(init_list = init_list, Y = Y, X = X,
+                                         R = R, Ddims= Ddims, idx = dim)
+        pre_init_list <- init_list
+        init_list[[dim]] <- y_reg_list$B3
+        lambdas <- y_reg_list$y_lambdas
+        if (convergence_func(pre_init_list, init_list, dim, convThresh)) {
+          converged <- TRUE
+          break
+        }
+      }
+    }
+  }
 }
