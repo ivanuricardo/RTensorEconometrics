ee634bbf0f749f8c6273513cea7d1a6b1c3a6fff
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-06-03 16:30:52 +0200

    initialize tucker regression

--- R/tucker_tmp.R
+++ R/tucker_tmp.R
@@ -1,3 +1,14 @@
+library(TensorEconometrics)
+
+set.seed(20230603)
+X <- rand_tensor(c(100, 10, 8))
+Y <- rand_tensor(c(100, 10, 8))
+R <- c(5,4,3,2)
+obs_dim_X <- 1
+obs_dim_Y <- 1
+convThresh <- 1e-04
+max_iter <- 400
+
 # Tucker Regression
 
 xt_regression <- function () {
@@ -12,6 +23,17 @@ tuck_conv <- function() {
   
 }
 
-tucker_regression <- function(Y, X, R, obs_dim_X, obs_dim_Y) {
+tucker_regression <- function(Y, X, R, obs_dim_X, obs_dim_Y, convThresh = 1e-05, 
+                              max_iter = 400, seed = 0) {
+  if (seed > 0) set.seed(seed)
+  
+  # Generate initial random tensor and random CP decomposition
+  init_B <- rand_tensor(c(X@modes[-obs_dim_X], Y@modes[-obs_dim_Y]))
+  init_list <- list(
+    matrix(rnorm(X@modes[2] * R), nrow = X@modes[2]),
+    matrix(rnorm(X@modes[3] * R), nrow = X@modes[3]),
+    matrix(rnorm(Y@modes[2] * R), nrow = Y@modes[2]),
+    matrix(rnorm(Y@modes[3] * R), nrow = Y@modes[3])
+  )
   
 }
