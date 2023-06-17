b1502200ab958edf23ab73f7b292410fe7cf7bc5
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-06-17 14:55:32 +0200

    New Token

--- R/tensor_regressions.R
+++ R/tensor_regressions.R
@@ -409,3 +409,5 @@ tucker_regression <- function(Y, X, R, convThresh = 1e-04, max_iter = 400,
   return(list(factors = init_list, B = tucker_rebuild(init_list), 
               num_iter = num_iter, converged = converged))
 }
+
+R <- c(2,3,4,5)
